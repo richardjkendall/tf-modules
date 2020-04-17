f49337e54355e64d2f95ce825ec62e2e03d6b916
Richard Kendall <richard.kendall@gmail.com>
2020-04-17 19:33:21 +1000

    atlantis-fargate: supports external alb

--- modules/atlantis-fargate/main.tf
+++ modules/atlantis-fargate/main.tf
@@ -60,21 +60,21 @@ data "aws_iam_policy_document" "service_role_policy" {
 }
 
 resource "aws_iam_policy" "service_role_policy" {
-  policy = "${data.aws_iam_policy_document.service_role_policy.json}"
+  policy = data.aws_iam_policy_document.service_role_policy.json
 }
 
 resource "aws_iam_role" "service_role" {
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_service_role_assume_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.ecs_service_role_assume_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "service_managed_ecs_policy_attach" {
-  role       = "${aws_iam_role.service_role.name}"
+  role       = aws_iam_role.service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "service_role_policy_attach" {
-  role       = "${aws_iam_role.service_role.name}"
-  policy_arn = "${aws_iam_policy.service_role_policy.arn}"
+  role       = aws_iam_role.service_role.name
+  policy_arn = aws_iam_policy.service_role_policy.arn
 }
 
 data "aws_iam_policy_document" "deployment_role_assume_policy" {
@@ -83,7 +83,7 @@ data "aws_iam_policy_document" "deployment_role_assume_policy" {
     effect = "Allow"
 
     principals {
-      identifiers = ["${aws_iam_role.service_role.arn}"]
+      identifiers = [aws_iam_role.service_role.arn]
       type        = "AWS"
     }
 
@@ -93,7 +93,7 @@ data "aws_iam_policy_document" "deployment_role_assume_policy" {
 
 resource "aws_iam_role" "deployment_role" {
   name               = "atlantis_terragrunt_deployment_role"
-  assume_role_policy = "${data.aws_iam_policy_document.deployment_role_assume_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.deployment_role_assume_policy.json
 }
 
 data "aws_iam_policy_document" "deployment_role" {
@@ -192,23 +192,23 @@ data "aws_iam_policy_document" "deployment_role" {
 }
 
 resource "aws_iam_policy" "deployment_role_policy" {
-  policy = "${data.aws_iam_policy_document.deployment_role.json}"
+  policy = data.aws_iam_policy_document.deployment_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "deployment_role_policy_attach" {
-  role       = "${aws_iam_role.deployment_role.name}"
-  policy_arn = "${aws_iam_policy.deployment_role_policy.arn}"
+  role       = aws_iam_role.deployment_role.name
+  policy_arn = aws_iam_policy.deployment_role_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "user_deployment_policies" {
   count       = length(var.deployment_role_policies)
 
-  role        = "${aws_iam_role.deployment_role.name}"
+  role        = aws_iam_role.deployment_role.name
   policy_arn  = element(var.deployment_role_policies, count.index)
 }
 
 resource "aws_ecs_cluster" "cluster" {
-  name = "${var.ecs_cluster_name}"
+  name = var.ecs_cluster_name
 }
 
 resource "aws_cloudwatch_log_group" "logs" {
@@ -222,45 +222,47 @@ resource "random_string" "random_secret" {
 
 resource "aws_ecs_task_definition" "task" {
   depends_on    = [aws_cloudwatch_log_group.logs]
-  family        = "${var.task_def_name}"
+  family        = var.task_def_name
 
-  container_definitions = "${templatefile("${path.module}/atlantis.json", {
-    gh_user           = "${var.gh_user}"
-    whitelist         = "${var.gh_repo_whitelist}"
+  container_definitions = templatefile("${path.module}/atlantis.json", {
+    gh_user           = var.gh_user
+    whitelist         = var.gh_repo_whitelist
     url               = "https://${var.host_name}.${var.root_domain}"
-    iam_role          = "${aws_iam_role.deployment_role.arn}"
+    iam_role          = aws_iam_role.deployment_role.arn
     repoconfig        = "${jsonencode(file("${path.module}/repoconfig.json"))}"
-    gh_webhook_secret = "${var.gh_webhook_secret_name}"
-    gh_token          = "${var.gh_token_secret_name}"
-    region            = "${var.aws_region}"
+    gh_webhook_secret = var.gh_webhook_secret_name
+    gh_token          = var.gh_token_secret_name
+    region            = var.aws_region
     log_group         = "/etc/atlantis"
-    random_secret     = "${random_string.random_secret.result}"
-  })}"
+    random_secret     = random_string.random_secret.result
+    cpu               = var.cpu
+    memory            = var.memory
+  })
 
-  execution_role_arn        = "${aws_iam_role.service_role.arn}"
-  task_role_arn             = "${aws_iam_role.service_role.arn}"
+  execution_role_arn        = aws_iam_role.service_role.arn
+  task_role_arn             = aws_iam_role.service_role.arn
   requires_compatibilities  = ["FARGATE"]
   network_mode              = "awsvpc"
-  cpu                       = 256
-  memory                    = 512
+  cpu                       = var.cpu
+  memory                    = var.memory
 }
 
 resource "aws_ecs_service" "service" {
   name              = "atlantis"
-  cluster           = "${aws_ecs_cluster.cluster.id}"
-  task_definition   = "${aws_ecs_task_definition.task.arn}"
+  cluster           = aws_ecs_cluster.cluster.id
+  task_definition   = aws_ecs_task_definition.task.arn
   desired_count     = 1
   launch_type       = "FARGATE"
 
   load_balancer {
     container_name    = "atlantis"
     container_port    = "4141"
-    target_group_arn  = "${aws_lb_target_group.target_group.arn}"
+    target_group_arn  = aws_lb_target_group.target_group.arn
   }
 
   network_configuration {
     subnets           = var.task_subnets
-    security_groups   = ["${aws_security_group.task_p_4141.id}"]
+    security_groups   = [aws_security_group.task_p_4141.id]
   }
 }
 
@@ -268,11 +270,26 @@ resource "aws_security_group" "task_p_4141" {
   name        = "allow_port_4141_for_task"
   description = "Allow Atlantis port 4141 from LB"
 
-  ingress {
-    from_port       = 4141
-    to_port         = 4141
-    protocol        = "tcp"
-    security_groups = ["${aws_security_group.allow_tls.id}"]
+  dynamic "ingress" {
+    for_each = var.create_lb == true ? ["b"] : []
+
+    content {
+      from_port       = 4141
+      to_port         = 4141
+      protocol        = "tcp"
+      security_groups = [aws_security_group.allow_tls.0.id]
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = var.create_lb == true ? [] : ["b"]
+
+    content {
+      from_port       = 4141
+      to_port         = 4141
+      protocol        = "tcp"
+      security_groups = [var.lb_sec_group_id]
+    }
   }
 
   egress {
@@ -284,6 +301,8 @@ resource "aws_security_group" "task_p_4141" {
 }
 
 resource "aws_security_group" "allow_tls" {
+  count = var.create_lb == true ? 1 : 0
+
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 
@@ -303,30 +322,59 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_lb" "lb" {
+  count = var.create_lb == true ? 1 : 0
+
   internal            = false
   load_balancer_type  = "application"
-  security_groups     = ["${aws_security_group.allow_tls.id}"]
+  security_groups     = [aws_security_group.allow_tls.0.id]
   subnets             = var.lb_subnets
 }
 
 resource "aws_lb_target_group" "target_group" {
   port                  = "4141"
   protocol              = "HTTP"
-  vpc_id                = "${var.vpc_id}"
+  vpc_id                = var.vpc_id
   target_type           = "ip"
   deregistration_delay  = 10
 }
 
 resource "aws_lb_listener" "alb_listener" {
-  load_balancer_arn = "${aws_lb.lb.arn}"
+  count = var.create_lb == true ? 1 : 0
+
+  load_balancer_arn = aws_lb.lb.0.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "${aws_acm_certificate.listener_cert.arn}"
+  certificate_arn   = aws_acm_certificate.listener_cert.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_listener_certificate" "lb_cert_attach" {
+  count = var.create_lb == true ? 0 : 1
+
+  listener_arn    = var.listener_arn
+  certificate_arn = aws_acm_certificate.listener_cert.arn
+}
+
+resource "aws_lb_listener_rule" "lb_listener_rule" {
+  count = var.create_lb == true ? 0 : 1
+
+  listener_arn = var.listener_arn
+  priority     = var.rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.host_name}.${var.root_domain}"]
+    }
   }
 }
 
@@ -336,26 +384,48 @@ resource "aws_acm_certificate" "listener_cert" {
 }
 
 resource "aws_route53_record" "listener_cert_validation" {
-  name    = "${aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.root_zone.id}"
-  records = ["${aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_value}"]
+  name    = aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.root_zone.id
+  records = [aws_acm_certificate.listener_cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = "${aws_acm_certificate.listener_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.listener_cert_validation.fqdn}"]
+  certificate_arn         = aws_acm_certificate.listener_cert.arn
+  validation_record_fqdns = [aws_route53_record.listener_cert_validation.fqdn]
 }
 
 resource "aws_route53_record" "r53_alb" {
-  zone_id = "${data.aws_route53_zone.root_zone.zone_id}"
+  count = var.create_lb == true ? 1 : 0
+
+  zone_id = data.aws_route53_zone.root_zone.zone_id
   name    = "${var.host_name}.${var.root_domain}"
   type    = "A"
 
   alias {
-    name                   = "${aws_lb.lb.dns_name}"
-    zone_id                = "${aws_lb.lb.zone_id}"
+    name                   = aws_lb.lb.0.dns_name
+    zone_id                = aws_lb.lb.0.zone_id
+    evaluate_target_health = true
+  }
+}
+
+data "aws_lb" "existing_lb" {
+  count = var.create_lb == true ? 0 : 1
+
+  arn = var.lb_arn
+}
+
+resource "aws_route53_record" "r53_existing_alb" {
+  count = var.create_lb == true ? 0 : 1
+
+  zone_id = data.aws_route53_zone.root_zone.zone_id
+  name    = "${var.host_name}.${var.root_domain}"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_lb.existing_lb.0.dns_name
+    zone_id                = data.aws_lb.existing_lb.0.zone_id
     evaluate_target_health = true
   }
 }
--- modules/atlantis-fargate/variables.tf
+++ modules/atlantis-fargate/variables.tf
@@ -9,6 +9,18 @@ variable "ecs_cluster_name" {
   type = string
 }
 
+variable "cpu" {
+  description = "CPU units for the task"
+  default = 256
+  type = number
+}
+
+variable "memory" {
+  description = "memory for the task"
+  default = 512
+  type = number
+}
+
 variable "task_def_name" {
   default = "atlantis"
   description = "name of the task definition created for atlantis"
@@ -60,6 +72,7 @@ variable "allowed_ips" {
 variable "lb_subnets" {
   type = list(string)
   description = "subnets for the load balancer, should have public IP assignment possible + IGW attached"
+  default = []
 }
 
 variable "task_subnets" {
@@ -71,4 +84,34 @@ variable "deployment_role_policies" {
   type = list(string)
   default = []
   description = "list of arns of custom deployment role policies to be added"
+}
+
+variable "create_lb" {
+  description = "should the module create a load balancer or link to an existing one"
+  default = true
+  type = bool
+}
+
+variable "lb_arn" {
+  description = "arn of existing load balancer if linking to an existing lb"
+  default = ""
+  type = string
+}
+
+variable "listener_arn" {
+  description = "arn of existing load balancer listener if linking to an existing lb"
+  default = ""
+  type = string
+}
+
+variable "rule_priority" {
+  description = "priority used for rule on existing alb"
+  default = 100
+  type = number
+}
+
+variable "lb_sec_group_id" {
+  description = "id of security group attached to existing alb"
+  default = ""
+  type = string
 }
