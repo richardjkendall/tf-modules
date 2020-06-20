8cf3d5440a55eebcba4c2685ea769f99d503f62a
Richard Kendall <richard.kendall@gmail.com>
2020-06-20 19:21:48 +1000

    azure-clone-to-s3: adding cloudtrail

--- modules/azure-clone-to-s3/main.tf
+++ modules/azure-clone-to-s3/main.tf
@@ -45,6 +45,96 @@ resource "aws_s3_bucket_public_access_block" "block_build_bucket_pub_access" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket" "trail_bucket" {
+  bucket_prefix = "cloudtrail"
+  force_destroy = true
+
+  dynamic "server_side_encryption_configuration" {
+    for_each = var.encrypt_buckets == true ? [ "blah" ] : []
+
+    content {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "trail_bucket_policy" {
+  statement {
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.trail_bucket.bucket}"
+    ]
+
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+
+    principals {
+      identifiers = ["cloudtrail.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.trail_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+    
+    actions = [
+      "s3:PutObject"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "trail_bucket_policy" {
+  bucket = aws_s3_bucket.trail_bucket.id
+  policy = data.aws_iam_policy_document.trail_bucket_policy.json
+}
+
+resource "aws_s3_bucket_public_access_block" "block_trail_bucket_pub_access" {
+  bucket = aws_s3_bucket.trail_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudtrail" "s3_events_trail" {
+  name                          = "azure-cp-s3-trail"
+  s3_bucket_name                = aws_s3_bucket.trail_bucket.id
+  include_global_service_events = false
+
+  event_selector {
+    read_write_type           = "WriteOnly"
+    include_management_events = false
+
+    data_resource {
+      type = "AWS::S3::Object"
+
+      values = ["${aws_s3_bucket.build_bucket.arn}/"]
+    }
+  }
+}
+
 data "aws_iam_policy_document" "api_policy" {
   statement {
     sid = "1"
