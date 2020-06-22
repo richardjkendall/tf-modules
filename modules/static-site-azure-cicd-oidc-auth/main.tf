1947743a9968d3e362fe4bd030d93bfda052317e
Richard Kendall <richard.kendall@gmail.com>
2020-06-22 22:32:19 +1000

    static-site-azure-cicd-oidc-auth: adding first version

---
+++ modules/static-site-azure-cicd-oidc-auth/main.tf
@@ -0,0 +1,72 @@
+/*
+title: static-site-azure-cicd-oidc-auth
+desc: Deploys a simple static site on CloudFront backed by an S3 origin with CICD from azure devops and protected by OIDC based login.
+partners: azure-clone-to-s3
+depends: static-site-azure-cicd, lambda-function
+*/
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  domain_root_wo_dots = replace(var.domain_root, ".", "-")
+  sitename_prefix_wo_dots = replace(var.sitename_prefix, ".", "-")
+}
+
+module "static_site" {
+  source              = "../static-site-azure-cicd/"
+
+  /* static site */
+  sitename_prefix     = var.sitename_prefix
+  domain_root         = var.domain_root
+  aws_region          = var.aws_region
+  access_log_bucket   = var.access_log_bucket
+  access_log_prefix   = var.access_log_prefix
+  encrypt_buckets     = var.encrypt_buckets
+
+  /* cicd */
+  allow_root          = var.allow_root
+  build_image         = var.build_image
+  source_s3_bucket    = var.source_s3_bucket
+  source_s3_prefix    = var.source_s3_prefix
+
+  /* auth related */
+  viewer_req_edge_lambda_arns = [
+    module.lambda.qualified_arn
+  ]
+}
+
+module "lambda" {
+  source = "../lambda-function"
+
+  /* need to provision in us-east-1 for lambda@edge to work */
+  aws_region        = "us-east-1"
+
+  function_name     = "jwtauth-${local.sitename_prefix_wo_dots}-${local.domain_root_wo_dots}-${var.source_s3_prefix}"
+  function_handler  = "lambda.lambda_handler"
+  runtime           = "python3.7"
+  memory            = 128
+  timeout           = 5
+
+  environment_variables = {}
+
+  code_repository         = "https://github.com/richardjkendall/cf-edge-jwt.git"
+  execution_role_policies = []
+  publish                 = true
+
+  build_environment = {
+    HOST            = var.keycloak_host
+    REALM           = var.realm 
+    CLIENT_ID       = var.client_id
+    CLIENT_SECRET   = var.client_secret 
+    AUTH_COOKIE     = var.auth_cookie_name
+    REFRESH_COOKIE  = var.refresh_cookie_name
+    REDIRECT_URI    = "https://${var.sitename_prefix}.${var.domain_root}/_login"
+    VAL_API_URL     = var.val_api_url
+  }
+}
---
+++ modules/static-site-azure-cicd-oidc-auth/variables.tf
@@ -0,0 +1,89 @@
+variable "aws_region" {
+  description = "region where provisioning should happen"
+  type = string
+}
+
+variable "sitename_prefix" {
+  description = "prefix of site name e.g. for www.example.com this would be www"
+  type = string
+}
+
+variable "domain_root" {
+  description = "domain root for site e.g. example.com.  This must be available in Route53."
+  type = string
+}
+
+variable "access_log_bucket" {
+  description = "S3 bucket where access logs will be placed"
+  type = string
+}
+
+variable "access_log_prefix" {
+  description = "prefix used for any access logs written to S3"
+  type = string
+}
+
+variable "encrypt_buckets" {
+  type = bool
+  default = false
+  description = "encrypt buckets with default AWS keys"
+}
+
+variable "allow_root" {
+  type = bool
+  default = false
+  description = "allow build process to become root (sudo)"
+}
+
+variable "build_image" {
+  type = string
+  default = "aws/codebuild/standard:2.0"
+  description = "what build image should be used to run the build job"
+}
+
+variable "source_s3_bucket" {
+  description = "S3 bucket which is the source for the build process"
+  type = string
+}
+
+variable "source_s3_prefix" {
+  description = "S3 bucket prefix used for the source build zip file"
+  type = string
+}
+
+variable "keycloak_host" {
+  type = string
+  description = "name of keycloak host"
+}
+
+variable "realm" {
+  type = string
+  description = "keycloak auth realm"
+}
+
+variable "client_id" {
+  type = string
+  description = "client ID for keycloak client"
+}
+
+variable "client_secret" {
+  type = string
+  description = "client secret for keycloak client"
+}
+
+variable "auth_cookie_name" {
+  type = string
+  description = "name of cookie used to hold auth token"
+  default = "auth"
+}
+
+variable "refresh_cookie_name" {
+  type = string
+  description = "name of cookie used to hold refresh token"
+  default = "rt"
+}
+
+variable "val_api_url" {
+  type = string
+  description = "URL for JWT validation API"
+}
