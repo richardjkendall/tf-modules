553a12cceb98f606e90791f188736f587811de32
Richard Kendall <richard.kendall@gmail.com>
2020-10-01 12:54:06 +1000

    static-site: updating to match latest aws provider for acm validation

--- modules/static-site/main.tf
+++ modules/static-site/main.tf
@@ -171,17 +171,26 @@ resource "aws_acm_certificate" "endpoint_cert" {
 }
 
 resource "aws_route53_record" "endpoint_cert_validation" {
-  name    = aws_acm_certificate.endpoint_cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.endpoint_cert.domain_validation_options.0.resource_record_type
-  zone_id = data.aws_route53_zone.root_zone.id
-  records = [aws_acm_certificate.endpoint_cert.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.endpoint_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.root_zone.id
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.endpoint_cert.arn
-  validation_record_fqdns = [aws_route53_record.endpoint_cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.endpoint_cert_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "cf_endpoint_domain_r53" {
