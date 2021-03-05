13ae0e5cf934ef113362864b98e4ec9c0f3ff815
Richard Kendall <richard.kendall@gmail.com>
2021-03-05 13:29:33 +1100

    pipeline-viewer: new module added

---
+++ modules/pipeline-viewer/main.tf
@@ -0,0 +1,98 @@
+/*
+title: pipeline-viewer
+desc: Deploys an application to view codepipeline/codebuild status.
+depends: ecs-service
+*/
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "aws_iam_policy_document" "pipe_viewer_policy" {
+  statement {
+    sid = "1"
+    effect = "Allow"
+    actions = [
+      "codepipeline:ListPipelines",
+      "codepipeline:GetPipelineState"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "2"
+    effect = "Allow"
+    actions = [
+      "codebuild:BatchGetBuilds"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "3"
+    effect = "Allow"
+    actions = [
+      "logs:GetLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "pipe_viewer_policy" {
+  policy = data.aws_iam_policy_document.pipe_viewer_policy.json
+}
+
+module "service" {
+  source = "../ecs-service"
+
+  aws_region                    = var.aws_region
+  cluster_name                  = var.cluster_name
+  service_name                  = var.service_name
+  service_registry_id           = var.service_registry_id
+  service_registry_service_name = var.service_registry_service_name
+
+  task_name          = "proxy"
+  image              = "not used"
+  cpu                = 256
+  memory             = 128+256
+  network_mode       = "bridge"
+  number_of_tasks    = 1
+
+  port_mappings = [
+    {
+      containerPort = 80
+      hostPort = 0
+      protocol = "tcp"
+    }
+  ]
+
+  task_def_override = templatefile("${path.module}/tasks.json", {
+    region                      = var.aws_region
+    cluster                     = var.cluster_name
+    service                     = var.service_name
+    
+    metadata_url                = var.metadata_url
+    jwks_uri                    = var.jwks_uri
+    client_id                   = var.client_id
+    domain                      = var.domain
+    port                        = var.port
+    scheme                      = var.scheme
+    client_secret_ssm_name      = var.client_secret_ssm_name
+    crypto_passphrase_ssm_name  = var.crypto_passphrase_ssm_name
+  })
+
+  task_role_policies = [
+    aws_iam_policy.pipe_viewer_policy.arn
+  ]
+
+}
---
+++ modules/pipeline-viewer/variables.tf
@@ -0,0 +1,68 @@
+variable "aws_region" {
+  description = "region where provisioning should happen"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "name of cluster where service will run"
+  type = string
+}
+
+variable "service_name" {
+  description = "name of ECS service"
+  type = string
+  default = "user-manager"
+}
+
+variable "service_registry_id" {
+  description = "ID for the AWS service discovery namespace we will use"
+  type = string
+}
+
+variable "service_registry_service_name" {
+  description = "name for service we will use in the service registry"
+  type = string
+}
+
+variable "metadata_url" {
+  description = "OIDC idp metadata url"
+  type = string
+}
+
+variable "jwks_uri" {
+  description = "OIDC idp web key set uri"
+  type = string
+}
+
+variable "client_id" {
+  description = "OIDC client ID"
+  type = string
+}
+
+variable "domain" {
+  description = "domain name for the proxy service"
+  type = string
+}
+
+variable "port" {
+  description = "port where proxy service is exposed, typically 443"
+  type = string
+  default = "443"
+}
+
+variable "scheme" {
+  description = "URL scheme used for proxy endpoint, should be https"
+  type = string
+  default = "https"
+}
+
+variable "client_secret_ssm_name" {
+  description = "name of SSM parameter which contains OIDC client secret"
+  type = string
+}
+
+variable "crypto_passphrase_ssm_name" {
+  description = "name of SSM parameter which contains OIDC crypto passphrase"
+  type = string
+}
+
